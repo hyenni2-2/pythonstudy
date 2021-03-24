8e42bd4b723b831ee28df395ae2cdcbeb0a72066
HyeinChoi <hi.choi1031@gmail.com>
2021-03-24 22:23:45 +0900

    210324 *표찍기

--- Doit1-3.py
+++ Doit1-3.py
@@ -1,40 +1,56 @@
 # a부터 b까지 정수의 합을 구하기(for문)위해 값 정렬
 
-print('a부터 b까지 정수의 합을 구합니다.')
-a = int(input('정수 a를 입력하세요.: '))
-b = int(input('정수 b를 입력하세요.: '))
+# print('a부터 b까지 정수의 합을 구합니다.')
+# a = int(input('정수 a를 입력하세요.: '))
+# b = int(input('정수 b를 입력하세요.: '))
 
-if a > b:
-    a, b = b, a
+# if a > b:
+#     a, b = b, a
 
-sum = 0
-for i in range(a, b + 1):
-    sum += i
+# sum = 0
+# for i in range(a, b + 1):
+#     sum += i
 
-print(f'{a}부터 {b}까지 정수의 합은 {sum}입니다.')
+# print(f'{a}부터 {b}까지 정수의 합은 {sum}입니다.')
 
 
 # +와 -를 번갈아가며 출력하기(내 코드)
-print('+와 -를 번갈아 출력합니다.')
-n = int(input('몇 개를 출력할까요? : '))
+# print('+와 -를 번갈아 출력합니다.')
+# n = int(input('몇 개를 출력할까요? : '))
 
-for i in range(1,n + 1):
-    if i % 2:
-        print('+',end='')
-    else:
-        print('-',end='')
+# for i in range(1,n + 1):
+#     if i % 2:
+#         print('+',end='')
+#     else:
+#         print('-',end='')
 
-print()
+# print()
 
 # +와 -를 번갈아가며 출력하기(교재 코드)
-print('+와 -를 번갈아 출력합니다.')
-n = int(input('몇 개를 출력할까요? : '))
+# print('+와 -를 번갈아 출력합니다.')
+# n = int(input('몇 개를 출력할까요? : '))
 
-for _ in range(n // 2):
-    print('+-',end='')
+# for _ in range(n // 2):
+#     print('+-',end='')
 
-if n % 2:
-    print('+', end='')
+# if n % 2:
+#     print('+', end='')
+
+# print()
+
+# *를 n개 출력하되, w개마다 줄바꿈 하기
+print('*를 출력합니다.')
+n = int(input('몇 개를 출력할까요?: '))
+w = int(input('몇 개마다 줄바꿈을 할까요?: '))
+etc = 0
+
+for _ in range(n // w):
+    print('*' * w)
+
+if n % w:
+    etc = (n % w)
+    print('*' * etc)
 
 print()
+
 
